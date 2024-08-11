4a0c621967863b016be80b9349278ae4fad3c9b8
Ahmet Talha Biçer <bicerahmettalha@gmail.com>
2024-08-11 12:05:36 +0300

    update docker

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc793fcc7a95c6ec6492aee698daed0bebc1714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3be8393cf75af7612ff81086135e9866871170")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
