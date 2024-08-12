b1fdc958212e750534904df21d14501a72226986
Ahmet Talha Biçer <bicerahmettalha@gmail.com>
2024-08-12 13:06:58 +0300

    docker update

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0c621967863b016be80b9349278ae4fad3c9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2905dc7017977c5173b4e9a0f50a98b3562c32dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe466a06ff24534ea5508db205e29a343301bafc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2905dc7017977c5173b4e9a0f50a98b3562c32dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
